64267ec3e7e91223e6649ab72a434627c669e664
santhu92 <123493413+santhu92@users.noreply.github.com>
2023-05-28 09:07:06 +0530

    Update app.py
    removed my creds/connection URL in the app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017")
-database = "youtubescrp1"
-user="root"
-pw="root123"
-engine1 = create_engine("mysql+pymysql://{user}:{pw}@localhost".format(user="root", pw="root123"))
-engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root", pw="root123", db = database))
+client = MongoClient("mongodb://<hostname>:<port>")   #### please replace the db connection url as per your mongo db installed ('mongodb://localhost:27017')
+database = "youtubescrp1"  #### keep this db name as it is dont change this
+user=    #### replace it with your username for example user="root"
+pw=   #### replace it with your password for example pw="root123"
+engine1 = create_engine("mysql+pymysql://{user}:{pw}@<hostname>".format(user="xxxxx", pw="xxxxx123"))   #### replace hostname and username as per your installation 
+engine = create_engine("mysql+pymysql://{user}:{pw}@<hostname>/{db}".format(user="xxxxx", pw="xxxxx123", db = database)) #### replace hostname and username as per your installation 
