382c269fe93c657954da657f2dfacd94483df79c
santhu92 <123493413+santhu92@users.noreply.github.com>
2023-05-28 10:15:03 +0530

    Update YTB_app.py
    Added new functionality to install the required libraries automatically.

--- YTB_app.py
+++ YTB_app.py
@@ -1,4 +1,13 @@
 import subprocess
+def install_libraries():
+    try:
+        subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
+        return print("Library installation successful!")
+    except subprocess.CalledProcessError as e:
+        return print("An error occurred while installing libraries:", e)
+
+# Call the install_libraries() function to install the libraries
+install_libraries()
 command = "streamlit run YOUTUBE_STREAMLIT_app.py"
 subprocess.Popen(['cmd', '/c', command])
 subprocess.wait()
